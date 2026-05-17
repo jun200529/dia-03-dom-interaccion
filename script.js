fb50cafd0b7af153fd2b1736b32e215cd78d1889
Juan Sebastian Osorio <veli0m.proyectos@gmail.com>
2026-05-16 19:48:13 -0500

    Día 3 - interacción DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+let boton = document.getElementById("boton");
+
+let titulo = document.getElementById("titulo");
+
+boton.addEventListener("click", function() {
+
+    titulo.textContent = "Ahora JavaScript cambió este texto";
+
+});
